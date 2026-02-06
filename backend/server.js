a4af7de14f76b03690523f8efdc2f0060cb3c452
Mohamed Mooge <mohamed@local.dev>
2026-02-06 01:15:59 -0800

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,6 @@ const connectDB = require("./config/db");
 connectDB();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running on " + PORT));
+
+// const PORT = process.env.PORT || 5010;
+// app.listen(PORT, () => console.log(`Server running on ${PORT}`));
