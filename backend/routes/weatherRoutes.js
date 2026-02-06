827b7b3c8e4ea95c8481a2d17191ce573fd917c4
musab-48 <musababdullahi0069@gmail.com>
2026-02-06 13:26:54 +0300

    Delete backend/routes directory

--- backend/routes/weatherRoutes.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { fetchWeather } = require("../controllers/weatherController");
-router.get("/", fetchWeather);
-module.exports = router;
